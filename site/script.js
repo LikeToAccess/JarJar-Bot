ee0b6d61d9cf67d4d380c1cbc219b87b90028a0b
LikeToAccess <37591675+LikeToAccess@users.noreply.github.com>
2021-06-28 05:28:38 -0500

    fixed everything

--- site/script.js
+++ site/script.js
@@ -1,19 +1,25 @@
 document.getElementById("discord_widget_id").onclick = function() {
-	//alert('The client has not been released yet.');
-	
 	url = "discord-invite";
 	window.open(
 		url, "JarJar Client Discord", "height=400, width=600, modal=yes, alwaysRaised=yes"
 	);
 };
 
-
 document.getElementById("download_link_id").onclick = function() {
-	// alert('The client has not been released yet.');
-	
-	//download_url = "https://www.dropbox.com/s/ixyo2l5bodc5bwg/placeholder.txt?dl=0";
-	// download_url = "https://en.wikipedia.org/wiki/Cheese";
+	alert("You need BETA for this! Enjoy some music instead?");
+
+	url = "https://youtu.be/dQw4w9WgXcQ";
 	window.open(
-		download_url, "JarJar Client Download", "height=800, width=600, modal=yes, alwaysRaised=yes"
+		url, "JarJar Download", "height=400, width=600, modal=yes, alwaysRaised=yes"
 	);
 };
+
+
+// document.getElementById("download_link_id").onclick = function() {
+// 	// alert("The client has not been released yet.");
+
+// 	// download_url = "https://en.wikipedia.org/wiki/Cheese";
+// 	window.open(
+// 		download_url, "JarJar Client Download", "height=800, width=600, modal=yes, alwaysRaised=yes"
+// 	);
+// };
